cc7c4b0a5a34d37c2c01cf5b859afb50365a9360
Zach <65182259+hydrabeer@users.noreply.github.com>
2024-09-09 09:57:37 -0400

    increment after print rather than before

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,14 +4,13 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 100) {
+        int i = 1;
+        while (i <= 100) {
             i = doFizzBuzz(i);
         }
     }
 
-    private static int doFizzBuzz(int i) {
-        i++;
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -20,18 +19,19 @@ class FizzBuzz {
         if (divisibleBy3 && divisibleBy5) {
 
             System.out.println("Fizz Buzz");
-
+            i++;
         } else if (divisibleBy3) {
 
             System.out.println("Fizz");
-
+            i++;
         } else if (divisibleBy5) {
 
             System.out.println("Buzz");
-
+            i++;
         } else {
 
             System.out.println(i);
+            i++;
         }
         return i;
     }
